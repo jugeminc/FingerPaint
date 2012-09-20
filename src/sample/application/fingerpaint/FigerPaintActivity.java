43525a721a22bd5012a7201efada4d96610b2840
ICT <ict@R1-00.local>
2012-09-20 21:58:12 +0900

    0920

--- src/sample/application/fingerpaint/FigerPaintActivity.java
+++ src/sample/application/fingerpaint/FigerPaintActivity.java
@@ -1,13 +1,19 @@
 package sample.application.fingerpaint;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.os.Bundle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.ViewManager;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class FigerPaintActivity extends Activity implements OnTouchListener{
 	
@@ -27,5 +33,27 @@ public class FigerPaintActivity extends Activity implements OnTouchListener{
 		
 		return Boolean.valueOf(true);
 		//return Boolean2.TRUE;
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+	    super.onCreate(savedInstanceState);
+	    this.setContentView(R.layout.fingerpaint);
+		
+		ImageView iv = (ImageView) this.findViewById(R.id.imageView1);
+		Object obj = this.getSystemService(Context.WINDOW_SERVICE);
+		WindowManager wm = (WindowManager)obj;
+		Display disp = wm.getDefaultDisplay();
+		
+		w = disp.getWidth();
+		h = disp.getHeight();
+		
+		bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		paint = new Paint();
+		path = new Path();
+		canvas = new Canvas();
+		
+		
+		
 	}
 }
